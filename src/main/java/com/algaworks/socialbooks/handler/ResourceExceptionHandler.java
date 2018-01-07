0832380501cbdc522cee6b416327203f9592ea09
Alexandre <alebarbieri09@gmail.com>
2018-01-07 17:33:56 -0200

    Evoluindo o recurso Autor

--- src/main/java/com/algaworks/socialbooks/handler/ResourceExceptionHandler.java
+++ src/main/java/com/algaworks/socialbooks/handler/ResourceExceptionHandler.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.algaworks.socialbooks.domain.DetalhesErro;
+import com.algaworks.socialbooks.services.exceptions.AutorExistenteException;
+import com.algaworks.socialbooks.services.exceptions.AutorNaoEncontradoException;
 import com.algaworks.socialbooks.services.exceptions.LivroNaoEncontradoException;
 
 // Permite que nós interceptemos todos os tipos de exceções que possam acontecer no nosso código 
@@ -22,8 +24,32 @@ public class ResourceExceptionHandler {
 		erro.setTitulo(e.getMessage());
 		erro.setMensagemDesenvolvedor("Localização das informações referentes a esse erro.");
 		erro.setTimestamp(System.currentTimeMillis());
+	
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
+	}
+	
+	
+	@ExceptionHandler(AutorExistenteException.class)
+	public ResponseEntity<DetalhesErro> handleAutorExistenteException
+	(AutorExistenteException e, HttpServletRequest request){
+		DetalhesErro erro = new DetalhesErro();
+		erro.setStatus(Long.valueOf(HttpStatus.CONFLICT.value()));
+		erro.setTitulo(e.getMessage());
+		erro.setMensagemDesenvolvedor("Localização das informações referentes a esse erro.");
+		erro.setTimestamp(System.currentTimeMillis());
 		
-		// build = constrói a entidade de resposta sem nada no body
+		return ResponseEntity.status(HttpStatus.CONFLICT).body(erro);
+	}
+	
+	@ExceptionHandler(AutorNaoEncontradoException.class)
+	public ResponseEntity<DetalhesErro> handleAutorNaoEncontradoException
+	(AutorNaoEncontradoException e, HttpServletRequest request){
+		DetalhesErro erro = new DetalhesErro();
+		erro.setStatus(Long.valueOf(HttpStatus.NOT_FOUND.value()));
+		erro.setTitulo(e.getMessage());
+		erro.setMensagemDesenvolvedor("Localização das informações referentes a esse erro.");
+		erro.setTimestamp(System.currentTimeMillis());
+		
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
 	}
 }
--- src/main/java/com/algaworks/socialbooks/services/AutoresService.java
+++ src/main/java/com/algaworks/socialbooks/services/AutoresService.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 
 import com.algaworks.socialbooks.domain.Autor;
 import com.algaworks.socialbooks.repository.AutoresRepository;
+import com.algaworks.socialbooks.services.exceptions.AutorExistenteException;
+import com.algaworks.socialbooks.services.exceptions.AutorNaoEncontradoException;
 
 @Service
 public class AutoresService {
@@ -16,6 +18,30 @@ public class AutoresService {
 	
 	public List<Autor> listar(){
 		return autoresRepository.findAll();
+	}
+	
+	public Autor salvar(Autor autor) {
+		if (autor.getId() != null) {
+			Autor a = autoresRepository.findOne(autor.getId());
+			
+			if (a != null) {
+				throw new AutorExistenteException("O autor " + autor.getId() + " já existe.");
+			} else {
+				autor.setId(null);
+			}
+		}
+		
+		return autoresRepository.save(autor);
+	}
+	
+	public Autor buscar(Long id) {
+		Autor autor = autoresRepository.findOne(id);
+		
+		if (autor == null) {
+			throw new AutorNaoEncontradoException("O autor " + id + " não foi encontrado.");
+		}
+		
+		return autor;
 	}
 	
 	
